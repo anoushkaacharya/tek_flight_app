afaed02f429d13a840315d13d0121939df172290
Anoushka <anoushkaaditiacharya@gmail.com>
2026-04-23 01:34:03 +0530

    connected to db

---
+++ backend/user_service/models/userModel.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.BIGINT,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    defaultValue: "USER",
+  },
+});
+
+module.exports = User;
--- backend/user_service/server.js
+++ backend/user_service/server.js
@@ -1,6 +1,10 @@
 const app = require("./app");
+const sequelize=require("./config/db");
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
+sequelize.sync().then(()=>{console.log("Database connected");
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+})
+.catch(error=>console.log(error));
